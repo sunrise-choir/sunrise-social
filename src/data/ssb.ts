a0e63e5ae008bd9202b1e0eedaa5cb4c6cbb077e
Michael Williams <michael.williams@enspiral.com>
2021-06-09 01:16:55 +1200

    async iterables

--- src/data/ssb.ts
+++ src/data/ssb.ts
@@ -1,7 +1,11 @@
 // import { InMemoryLRUCache } from 'apollo-server-caching'
 // import { ApolloError } from 'apollo-server-errors'
 
-import { Peer as SsbConnQueryPeer } from 'ssb-conn-query/lib/types'
+import i from 'pull-stream-to-async-iterator'
+import {
+  Address as SsbConnHubAddress,
+  ConnectionData as SsbConnHubConnectionData,
+} from 'ssb-conn-hub/lib/types'
 import { promisify as p } from 'util'
 
 import {
@@ -28,12 +32,37 @@ export function SsbData(config: SsbDataConfig) {
       const { id: feedId } = ssb.whoami()
       return feedId
     },
-    getPeerConnections(): Array<PeerConnection> {
-      return ssb.conn
-        .query()
-        .peersInConnection()
-        .map((ssbConnQueryPeer: SsbConnQueryPeer): PeerConnection => {
-          const [address, connection] = ssbConnQueryPeer
+    async getPeerConnections(): Promise<Array<PeerConnection>> {
+      for await (const peerConnection of this.onPeerConnections()) {
+        return peerConnection
+      }
+    },
+    async getProfileByFeedId(feedId: FeedId): Promise<Profile> {
+      await p(ssb.db.onDrain)('aboutSelf')
+      const profile = await ssb.db.getIndex('aboutSelf').getProfile(feedId)
+      return profile
+    },
+    normalizePeerConnectionState(
+      state: SsbConnHubConnectionData['state'],
+    ): PeerConnectionState {
+      switch (state) {
+        case 'connecting':
+          return PeerConnectionState.Connecting
+        case 'connected':
+          return PeerConnectionState.Connected
+        case 'disconnecting':
+          return PeerConnectionState.Disconnecting
+      }
+    },
+    async *onPeerConnections(): AsyncIterator<Array<PeerConnection>> {
+      type SsbConnPeer = [SsbConnHubAddress, SsbConnHubConnectionData]
+      const ssbConnPeersIter: AsyncIterator<Array<SsbConnPeer>> = i(
+        ssb.conn.peers(),
+      )
+      for await (const ssbConnPeers of ssbConnPeersIter) {
+        yield ssbConnPeers.map((ssbConnPeer: SsbConnPeer) => {
+          console.log('peer', ssbConnPeer)
+          const [address, connection] = ssbConnPeer
           console.log('ssb connection', connection)
           const { key: feedId, state, type, inferredType } = connection
           return {
@@ -45,24 +74,6 @@ export function SsbData(config: SsbDataConfig) {
             type: type || inferredType,
           }
         })
-    },
-    async getProfileByFeedId(feedId: FeedId): Promise<Profile> {
-      await p(ssb.db.onDrain)('aboutSelf')
-      const profile = await ssb.db.getIndex('aboutSelf').getProfile(feedId)
-      return profile
-    },
-    normalizePeerConnectionState(
-      state: SsbConnQueryPeer[1]['state'],
-    ): PeerConnectionState | undefined {
-      switch (state) {
-        case undefined:
-          return undefined
-        case 'connecting':
-          return PeerConnectionState.Connecting
-        case 'connected':
-          return PeerConnectionState.Connected
-        case 'disconnecting':
-          return PeerConnectionState.Disconnecting
       }
     },
   }
---
+++ src/types/pull.d.ts
@@ -0,0 +1,2 @@
+declare module 'pull-stream'
+declare module 'pull-stream-to-async-iterator'
